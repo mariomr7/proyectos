d61d410c50d62aceca78ae682c3d92d579151600
mariomr7 <mariomacho02@gmail.com>
2021-12-02 09:46:18 +0100

    Primer guardado

---
+++ src/Ejemplos/Main.java
@@ -0,0 +1,21 @@
+package Ejemplos;
+
+public class Main {
+
+public static void main (String[] args) {
+
+    System.out.println("Hello world");
+    int[] array = new int [3]
+    array[0] =123;
+    array[1] = 456;
+    array[2] = 789;
+
+    System.out.println(array[0]);
+    System.out.println(array[1]);
+    System.out.println(array[2]);
+
+
+}
+
+
+}
